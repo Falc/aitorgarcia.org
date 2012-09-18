c473082d8938f2bb25c26e665e5243229a2b6887
Aitor García <aitor.falc@gmail.com>
2012-09-18 16:07:50 +0200

    Create the PortfolioBundle and activate it.

---
+++ src/AitorGarcia/PortfolioBundle/Controller/DefaultController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AitorGarcia\PortfolioBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction() {}
+}
