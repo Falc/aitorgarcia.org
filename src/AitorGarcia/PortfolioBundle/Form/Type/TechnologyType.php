c488c3d21294a21e2328a805041c3db33096598c
Aitor García <aitor.falc@gmail.com>
2012-09-27 20:34:42 +0200

    [PortfolioBundle] Form\Type: Add TechnologyType.

---
+++ src/AitorGarcia/PortfolioBundle/Form/Type/TechnologyType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AitorGarcia\PortfolioBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class TechnologyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', 'text', array(
+            'label' => 'Nombre:',
+            'trim'  => true
+        ));
+    }
+    
+    public function getDefaultOptions(array $options)
+    {
+        return array(
+            'data_class' => 'AitorGarcia\PortfolioBundle\Entity\Technology'
+        );
+    }
+
+    public function getName()
+    {
+        return 'technology';
+    }
+}
