48a3edeccdb13e44fc59711b031f635b6ed6ace0
Aitor García <aitor.falc@gmail.com>
2012-09-27 20:35:51 +0200

    [PortfolioBundle] Controller\AdminTechnology: Implement createAction().

--- src/AitorGarcia/PortfolioBundle/Controller/AdminTechnologyController.php
+++ src/AitorGarcia/PortfolioBundle/Controller/AdminTechnologyController.php
@@ -3,6 +3,7 @@
 namespace AitorGarcia\PortfolioBundle\Controller;
 
 use AitorGarcia\PortfolioBundle\Entity\Technology;
+use AitorGarcia\PortfolioBundle\Form\Type\TechnologyType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdminTechnologyController extends Controller
@@ -20,6 +21,31 @@ class AdminTechnologyController extends Controller
 
     public function createAction()
     {
+        $technology = new Technology();
+
+        $form = $this->createForm(new TechnologyType(), $technology);
+
+        $request = $this->get('request');
+        if ($request->getMethod() === 'POST')
+        {
+            $form->bindRequest($request);
+
+            if ($form->isValid())
+            {
+                $entityManager = $this->get('doctrine')->getEntityManager();
+                $entityManager->persist($technology);
+                $entityManager->flush();
+
+                $request->getSession()->setFlash('notice', 'La tecnología ha sido creada correctamente');
+                
+                return $this->redirect($this->generateUrl('admin_technology_list'));
+            }
+        }
+
+        return $this->render(
+            'PortfolioBundle:Admin:technology_create.html.twig',
+            array('form' => $form->createView())
+        );
     }
 
     public function editAction()
