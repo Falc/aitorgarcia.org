110c2f81e32ab324ec78629c0ad96e970b3eea81
Aitor García <aitor.falc@gmail.com>
2012-09-27 17:14:59 +0200

    [PortfolioBundle] Controller: Add AdminTechnologyController.

---
+++ src/AitorGarcia/PortfolioBundle/Controller/AdminTechnologyController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AitorGarcia\PortfolioBundle\Controller;
+
+use AitorGarcia\PortfolioBundle\Entity\Technology;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AdminTechnologyController extends Controller
+{
+    public function listAction()
+    {
+    }
+
+    public function createAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
