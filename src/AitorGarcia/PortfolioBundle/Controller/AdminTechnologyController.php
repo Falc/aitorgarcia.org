967842eccc113dfaf0f70d2450a878545ae0ed51
Aitor García <aitor.falc@gmail.com>
2012-10-03 20:29:49 +0200

    [PortfolioBundle] Controller\AdminTechnology: Add some comments to createAction().

--- src/AitorGarcia/PortfolioBundle/Controller/AdminTechnologyController.php
+++ src/AitorGarcia/PortfolioBundle/Controller/AdminTechnologyController.php
@@ -21,23 +21,33 @@ class AdminTechnologyController extends Controller
 
     public function createAction()
     {
+        // Create a blank technology
         $technology = new Technology();
 
+        // Create the form and set the data
         $form = $this->createForm(new TechnologyType(), $technology);
 
+        // Get the request
         $request = $this->get('request');
+
+        // If the request method is POST, process the data
         if ($request->getMethod() === 'POST')
         {
+            // Bind the request
             $form->bindRequest($request);
 
+            // If the form data is valid:
             if ($form->isValid())
             {
+                // 1) Get the entity manager and persist the entity
                 $entityManager = $this->get('doctrine')->getEntityManager();
                 $entityManager->persist($technology);
                 $entityManager->flush();
 
+                // 2) Display a success message
                 $request->getSession()->setFlash('notice', 'La tecnología ha sido creada correctamente');
-                
+
+                // 3) Redirect the user to the technology list
                 return $this->redirect($this->generateUrl('admin_technology_list'));
             }
         }
