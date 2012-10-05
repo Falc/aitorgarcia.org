ddd36a7d4854b955231c0b6659866ddcbc4e2045
Aitor García <aitor.falc@gmail.com>
2012-10-06 00:43:59 +0200

    [PortfolioBundle] Controller\AdminProject: Implement createAction().

--- src/AitorGarcia/PortfolioBundle/Controller/AdminProjectController.php
+++ src/AitorGarcia/PortfolioBundle/Controller/AdminProjectController.php
@@ -2,6 +2,8 @@
 
 namespace AitorGarcia\PortfolioBundle\Controller;
 
+use AitorGarcia\PortfolioBundle\Entity\Project;
+use AitorGarcia\PortfolioBundle\Form\Type\ProjectType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdminProjectController extends Controller
@@ -19,6 +21,42 @@ class AdminProjectController extends Controller
 
     public function createAction()
     {
+        // Create a blank project
+        $project = new Project();
+
+        // Create the form and set the data
+        $form = $this->createForm(new ProjectType(), $project);
+
+        // Get the request
+        $request = $this->get('request');
+
+        // If the request method is POST, process the data
+        if ($request->getMethod() === 'POST')
+        {
+            // Bind the request
+            $form->bind($request);
+
+            // If the form data is valid:
+            if ($form->isValid())
+            {
+                // 1) Get the entity manager and persist the entity
+                $entityManager = $this->get('doctrine')->getEntityManager();
+                $entityManager->persist($project);
+                $entityManager->flush();
+
+                // 2) Display a success message
+                $request->getSession()->getFlashBag()->add('success', 'El proyecto ha sido creado correctamente');
+
+                // 3) Redirect the user to the project list
+                return $this->redirect($this->generateUrl('admin_project_list'));
+            }
+        }
+
+        // Render the form
+        return $this->render(
+            'PortfolioBundle:Admin:project_create.html.twig',
+            array('form' => $form->createView())
+        );
     }
 
     public function editAction()
