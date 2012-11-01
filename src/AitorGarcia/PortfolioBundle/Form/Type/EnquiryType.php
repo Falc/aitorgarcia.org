0518fdeed1b7d271fe74140bf81ad40bdadd1c9a
Aitor García <aitor.falc@gmail.com>
2012-11-01 01:17:54 +0100

    [PortfolioBundle] Form\Type\Enquiry: Remove the labels, they have been included in the translation files.

--- src/AitorGarcia/PortfolioBundle/Form/Type/EnquiryType.php
+++ src/AitorGarcia/PortfolioBundle/Form/Type/EnquiryType.php
@@ -10,22 +10,18 @@ class EnquiryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('name', 'text', array(
-            'label' => 'Nombre:',
             'trim'  => true
         ));
 
         $builder->add('email', 'email', array(
-            'label' => 'Email:',
             'trim'  => true
         ));
 
         $builder->add('subject', 'text', array(
-            'label' => 'Asunto:',
             'trim'  => true
         ));
 
         $builder->add('body', 'textarea', array(
-            'label' => 'Mensaje:',
             'trim'  => true
         ));
     }
