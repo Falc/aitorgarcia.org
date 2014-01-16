ddbcfdae8b25a8763c2b73d5c96842dd8be0ff9b
Aitor García <aitor.falc@gmail.com>
2014-01-16 21:02:49 +0100

    [App] Do not load blank service definition files.

--- src/AitorGarcia/ContactBundle/DependencyInjection/AitorGarciaContactExtension.php
+++ src/AitorGarcia/ContactBundle/DependencyInjection/AitorGarciaContactExtension.php
@@ -33,8 +33,5 @@ class AitorGarciaContactExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
     }
 }
--- src/AitorGarcia/UserBundle/DependencyInjection/AitorGarciaUserExtension.php
+++ src/AitorGarcia/UserBundle/DependencyInjection/AitorGarciaUserExtension.php
@@ -33,8 +33,5 @@ class AitorGarciaUserExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        //$loader->load('services.xml');
     }
 }
