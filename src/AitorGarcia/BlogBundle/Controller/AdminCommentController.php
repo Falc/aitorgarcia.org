38c01655cb044befa213cfe480fe8fe7d7325b94
Aitor García <aitor.falc@gmail.com>
2014-01-18 21:09:29 +0100

    [BlogBundle] Implement the comment edit action. Add CommentType.

--- src/AitorGarcia/BlogBundle/Controller/AdminCommentController.php
+++ src/AitorGarcia/BlogBundle/Controller/AdminCommentController.php
@@ -9,6 +9,8 @@
 
 namespace AitorGarcia\BlogBundle\Controller;
 
+use AitorGarcia\BlogBundle\Entity\Comment;
+use AitorGarcia\BlogBundle\Form\Type\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
@@ -31,6 +33,56 @@ class AdminCommentController extends Controller
             'AitorGarciaBlogBundle:Admin:comment_list.html.twig',
             array(
                 'comments' => $comments
+            )
+        );
+    }
+
+    /**
+     * Displays the "comment edit" form and processes it.
+     *
+     * @param   integer $id     The ID of the comment to edit.
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        // Find the selected comment
+        $comment = $em->getRepository('AitorGarciaBlogBundle:Comment')->find($id);
+
+        // If the comment does not exist, display an error message
+        if ($comment === null)
+        {
+            $errorMessage = $this->get('translator')->trans('comments.message.do_not_exist');
+            throw $this->createNotFoundException($errorMessage);
+        }
+
+        // Create the form and set the data
+        $form = $this->createForm(new CommentType, $comment);
+
+        $request = $this->getRequest();
+        $form->handleRequest($request);
+
+        // If the form data is valid:
+        if ($form->isValid())
+        {
+            // 1) Persist the entity
+            $em->persist($comment);
+            $em->flush();
+
+            // 2) Display a success message
+            $successMessage = $this->get('translator')->trans('comments.message.success_edition');
+            $request->getSession()->getFlashBag()->add('success', $successMessage);
+
+            // 3) Redirect the user to the comment list
+            return $this->redirect($this->generateUrl('blog_admin_comment_list'));
+        }
+
+        // Render the form
+        return $this->render(
+            'AitorGarciaBlogBundle:Admin:comment_edit.html.twig',
+            array(
+                'form'       => $form->createView(),
+                'comment_id' => $comment->getId()
             )
         );
     }
---
+++ src/AitorGarcia/BlogBundle/Form/Type/CommentType.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * This file contains the CommentType class.
+ *
+ * @author		Aitor García <aitor.falc@gmail.com>
+ * @copyright	2014 Aitor García <aitor.falc@gmail.com>
+ * @license		https://github.com/Falc/aitorgarcia.org/blob/master/LICENSE Simplified BSD License
+ */
+
+namespace AitorGarcia\BlogBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * This form type will be used for the comment edition forms.
+ */
+class CommentType extends AbstractType
+{
+    /**
+     * Builds the form.
+     *
+     * @param   \Symfony\Component\Form\FormBuilderInterface    $builder    The form builder.
+     * @param   array                                           $options    The options.
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('authorName', 'text', array(
+            'trim'  => true,
+            'attr'  => array(
+                'class' => 'form-control'
+            )
+        ));
+
+        $builder->add('authorEmail', 'email', array(
+            'trim'  => true,
+            'attr'  => array(
+                'class' => 'form-control'
+            )
+        ));
+
+        $builder->add('body', 'textarea', array(
+            'attr'  => array(
+                'class'      => 'form-control tinymce',
+                'data-theme' => 'advanced'
+            )
+        ));
+    }
+
+    /**
+     * Sets the default options for this type.
+     *
+     * @param   \Symfony\Component\OptionsResolver\OptionsResolverInterface  $resolver  The resolver for the options.
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AitorGarcia\BlogBundle\Entity\Comment'
+        ));
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return  string The name of this type.
+     */
+    public function getName()
+    {
+        return 'comment';
+    }
+}
