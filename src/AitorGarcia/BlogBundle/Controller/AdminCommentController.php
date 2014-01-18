c767c938345f3602ef6a1c429801b59f735d238a
Aitor García <aitor.falc@gmail.com>
2014-01-18 22:24:31 +0100

    [BlogBundle] Implement the comment delete action.

--- src/AitorGarcia/BlogBundle/Controller/AdminCommentController.php
+++ src/AitorGarcia/BlogBundle/Controller/AdminCommentController.php
@@ -86,4 +86,74 @@ class AdminCommentController extends Controller
             )
         );
     }
+
+    /**
+     * Displays the "comment delete" form and processes it.
+     *
+     * @param   integer $id     The ID of the comment to delete.
+     */
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        // Find the selected comment
+        $comment = $em->getRepository('AitorGarciaBlogBundle:Comment')->find($id);
+
+        // If the comment does not exist, display an error message
+        if ($comment === null)
+        {
+            $errorMessage = $this->get('translator')->trans('comments.message.do_not_exist');
+            throw $this->createNotFoundException($errorMessage);
+        }
+
+        // Create a "fake" form
+        $form = $this->createDeleteForm($id);
+
+        $request = $this->getRequest();
+
+        // If the cancel button was pressed, redirect the user to the comment list
+        if ($request->request->has('cancel') === true)
+        {
+            return $this->redirect($this->generateUrl('blog_admin_comment_list'));
+        }
+
+        $form->handleRequest($request);
+
+        // If the form data is valid:
+        if ($form->isValid())
+        {
+            // 1) Remove the entity
+            $em->remove($comment);
+            $em->flush();
+
+            // 2) Display a success message
+            $successMessage = $this->get('translator')->trans('comments.message.success_deletion');
+            $request->getSession()->getFlashBag()->add('success', $successMessage);
+
+            // 3) Redirect the user to the comment list
+            return $this->redirect($this->generateUrl('blog_admin_comment_list'));
+        }
+
+        // Render the form
+        return $this->render(
+            'AitorGarciaBlogBundle:Admin:comment_delete.html.twig',
+            array(
+                'form'           => $form->createView(),
+                'comment_id'     => $comment->getId(),
+                'comment_author' => $comment->getAuthorName()
+            )
+        );
+    }
+
+    /**
+     * Creates a helper delete form.
+     *
+     * @param   integer $id     The ID of the comment to delete.
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+                    ->add('id', 'hidden')
+                    ->getForm();
+    }
 }
