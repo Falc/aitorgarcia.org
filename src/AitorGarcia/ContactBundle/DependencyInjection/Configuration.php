f2900804a4f3b6d3a0e3d0ca4ba8fb0e7d60ebb5
Aitor García <aitor.falc@gmail.com>
2014-01-26 01:49:54 +0100

    [ContactBundle] Minor changes in the file docblocks.

--- src/AitorGarcia/ContactBundle/DependencyInjection/Configuration.php
+++ src/AitorGarcia/ContactBundle/DependencyInjection/Configuration.php
@@ -2,9 +2,9 @@
 /**
  * This file contains the Configuration class.
  *
- * @author		Aitor García <aitor.falc@gmail.com>
- * @copyright	2013 Aitor García <aitor.falc@gmail.com>
- * @license		https://github.com/Falc/aitorgarcia.org/blob/master/LICENSE Simplified BSD License
+ * @author      Aitor García (Falc) <aitor.falc@gmail.com>
+ * @copyright   2013 Aitor García (Falc) <aitor.falc@gmail.com>
+ * @licens      https://github.com/Falc/aitorgarcia.org/blob/master/LICENSE Simplified BSD License
  */
 
 namespace AitorGarcia\ContactBundle\DependencyInjection;
--- src/AitorGarcia/ContactBundle/Form/Type/EnquiryType.php
+++ src/AitorGarcia/ContactBundle/Form/Type/EnquiryType.php
@@ -2,9 +2,9 @@
 /**
  * This file contains the EnquiryType class.
  *
- * @author		Aitor García <aitor.falc@gmail.com>
- * @copyright	2012-2013 Aitor García <aitor.falc@gmail.com>
- * @license		https://github.com/Falc/aitorgarcia.org/blob/master/LICENSE Simplified BSD License
+ * @author      Aitor García (Falc) <aitor.falc@gmail.com>
+ * @copyright   2012-2013 Aitor García (Falc) <aitor.falc@gmail.com>
+ * @license     https://github.com/Falc/aitorgarcia.org/blob/master/LICENSE Simplified BSD License
  */
 
 namespace AitorGarcia\ContactBundle\Form\Type;
