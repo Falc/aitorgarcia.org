7aeddf38adab3bbf67ce98699f7dae4faf744e53
Aitor García <aitor.falc@gmail.com>
2014-01-26 01:50:11 +0100

    [MainBundle] Minor changes in the file docblocks.

--- src/AitorGarcia/MainBundle/AitorGarciaMainBundle.php
+++ src/AitorGarcia/MainBundle/AitorGarciaMainBundle.php
@@ -2,9 +2,9 @@
 /**
  * This file contains the MainBundle class.
  *
- * @author		Aitor García <aitor.falc@gmail.com>
- * @copyright	2012 Aitor García <aitor.falc@gmail.com>
- * @license		https://github.com/Falc/aitorgarcia.org/blob/master/LICENSE Simplified BSD License
+ * @author      Aitor García (Falc) <aitor.falc@gmail.com>
+ * @copyright   2012 Aitor García (Falc) <aitor.falc@gmail.com>
+ * @license     https://github.com/Falc/aitorgarcia.org/blob/master/LICENSE Simplified BSD License
  */
 
 namespace AitorGarcia\MainBundle;
--- src/AitorGarcia/MainBundle/DependencyInjection/AitorGarciaMainExtension.php
+++ src/AitorGarcia/MainBundle/DependencyInjection/AitorGarciaMainExtension.php
@@ -2,9 +2,9 @@
 /**
  * This file contains the MainExtension class.
  *
- * @author		Aitor García <aitor.falc@gmail.com>
- * @copyright	2012-2014 Aitor García <aitor.falc@gmail.com>
- * @license		https://github.com/Falc/aitorgarcia.org/blob/master/LICENSE Simplified BSD License
+ * @author      Aitor García (Falc) <aitor.falc@gmail.com>
+ * @copyright   2012-2014 Aitor García (Falc) <aitor.falc@gmail.com>
+ * @license     https://github.com/Falc/aitorgarcia.org/blob/master/LICENSE Simplified BSD License
  */
 
 namespace AitorGarcia\MainBundle\DependencyInjection;
